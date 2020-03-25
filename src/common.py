306567a53ae4adc1c347896d70d0b437c22f0c64
yeapht <yeapht@zs.com>
2020-03-25 11:43:40 +0800

    add common file

---
+++ src/common.py
@@ -0,0 +1,14 @@
+SUBSCRIBE_PATH = "../res/subscribe.txt"
+
+# SS_JSON_CFG = "../config/ss/config"
+SSR_JSON_CFG = "../config/ssr/config"
+
+
+# VEMSS_JSON_CFG = "../config/vmess/config"
+
+
+def get_subscribe_url():
+    file = open(SUBSCRIBE_PATH, 'r')
+    for line in file.readlines():
+        return line.strip(line[-1])
+    file.close()
